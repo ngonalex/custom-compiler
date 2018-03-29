6fbe87e6d63eedf7a8f8c2539c8d34153f883eab
Miroslav Gavrilov <mvg@ucsb.edu>
2018-03-29 04:13:30 -0700

    Changed from one to another error

--- src/rcc.cc
+++ src/rcc.cc
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv)
 {
-	int x = "Some foolish mistake";
+	int oops = "Some foolish mistake";
 	std::cout << "Hello world of " << argc << " arguments!" << std::endl;
 	return 0;
 }
