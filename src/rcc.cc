fb387d0f5f644ef4d019ed962404ef9bd015a894
Miroslav Gavrilov <mvg@ucsb.edu>
2018-03-29 04:14:48 -0700

    There should now be no mistakes

--- src/rcc.cc
+++ src/rcc.cc
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv)
 {
-	int oops = "Some foolish mistake";
+	auto oops = "Some foolish mistake";
 	std::cout << "Hello world of " << argc << " arguments!" << std::endl;
 	return 0;
 }
