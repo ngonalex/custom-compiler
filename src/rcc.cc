e016c0735f8f551a149534b5bd3edc137288e4f8
Miroslav Gavrilov <mvg@ucsb.edu>
2018-03-29 04:16:16 -0700

    Trying strings out for size

--- src/rcc.cc
+++ src/rcc.cc
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv)
 {
-	auto oops = "Some foolish mistake";
+	std::string oops = "Some foolish mistake";
 	std::cout << "Hello world of " << argc << " arguments!" << std::endl;
 	return 0;
 }
