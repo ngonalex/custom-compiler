1f18d11204d9ce9ec31ef746b61e18ae0a05a8ae
Miroslav Gavrilov <gavrilov.miroslav@gmail.com>
2018-04-02 22:16:46 -0700

    Added one more test

--- rcc/tests/magic_number.cc
+++ rcc/tests/magic_number.cc
@@ -3,5 +3,9 @@
 #include "rcc.h"
 
 TEST(MagicNumberTest, GetRCCMagicNumber) {
-	  EXPECT_EQ(GetRCCMagicNumber(), 0x12CC);
+	EXPECT_EQ(GetRCCMagicNumber(), 0x12CC);
+}
+
+TEST(MagicNumberTest, RCCMagicNumberPlusOne) {
+	EXPECT_EQ(GetRCCMagicNumber() + 1, 0x12CD);
 }
