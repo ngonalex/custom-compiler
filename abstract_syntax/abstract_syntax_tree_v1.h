afacff75d1031953e960509571f66a24744b8710
Miroslav Gavrilov <gavrilov.miroslav@gmail.com>
2018-04-12 22:34:22 -0700

    Added/modified abstract_syntax.

---
+++ abstract_syntax/abstract_syntax_tree_v1.h
@@ -0,0 +1,125 @@
+// This version of the abstract syntax consists of a single arithmetic
+// expression. The grammar for the abstract syntax is:
+//
+// n ∈ Integer
+//
+// ae ∈ ArithmeticExp ⟵ n | ae1 op ae2
+// op ∈ BinaryOperator ⟵ + | - | ⨯ | ÷
+//
+// prog ∈ Program ⟵ ae
+//
+// I.e., a program is an arithmetic expression.
+
+#ifndef ABSTRACT_SYNTAX_ABSTRACT_SYNTAX_TREE_V1_H_
+#define ABSTRACT_SYNTAX_ABSTRACT_SYNTAX_TREE_V1_H_
+
+#include "utility/memory.h"
+
+namespace cs160 {
+namespace abstract_syntax {
+namespace version_1 {
+
+// Forward declarations of abstract syntax tree node types for use in the
+// abstract syntax tree visitor.
+class IntegerExpr;
+class AddExpr;
+class SubtractExpr;
+class MultiplyExpr;
+class DivideExpr;
+
+// The visitor abstract base class for visiting abstract syntax trees.
+class AstVisitor {
+ public:
+  virtual ~AstVisitor() {}
+
+  // These should be able to change members of the visitor, thus not const
+  virtual void VisitIntegerExpr(const IntegerExpr& exp) = 0;
+  virtual void VisitAddExpr(const AddExpr& exp) = 0;
+  virtual void VisitSubtractExpr(const SubtractExpr& exp) = 0;
+  virtual void VisitMultiplyExpr(const MultiplyExpr& exp) = 0;
+  virtual void VisitDivideExpr(const DivideExpr& exp) = 0;
+};
+
+// The definition of the abstract syntax tree abstract base class.
+class AstNode {
+ public:
+  virtual ~AstNode() {}
+
+  virtual void Visit(AstVisitor* visitor) const = 0;
+};
+
+// An integer constant expression.
+class IntegerExpr : public AstNode {
+ public:
+  explicit IntegerExpr(int value) : value_(value) {}
+
+  void Visit(AstVisitor* visitor) const { visitor->VisitIntegerExpr(*this); }
+
+  int value() const { return value_; }
+
+ private:
+  // The value of the integer constant.
+  int value_;
+};
+
+// An abstract binary operator node.
+class BinaryOperatorExpr : public AstNode {
+ public:
+  BinaryOperatorExpr(std::unique_ptr<const AstNode> lhs,
+                     std::unique_ptr<const AstNode> rhs)
+      : lhs_(std::move(lhs)), rhs_(std::move(rhs)) {}
+
+  const AstNode& lhs() const { return *lhs_; }
+  const AstNode& rhs() const { return *rhs_; }
+
+ protected:
+  // The left-hand side and right-hand side of the expression.
+  const std::unique_ptr<const AstNode> lhs_;
+  const std::unique_ptr<const AstNode> rhs_;
+};
+
+// An addition expression.
+class AddExpr : public BinaryOperatorExpr {
+ public:
+  AddExpr(std::unique_ptr<const AstNode> lhs,
+          std::unique_ptr<const AstNode> rhs)
+      : BinaryOperatorExpr(std::move(lhs), std::move(rhs)) {}
+
+  void Visit(AstVisitor* visitor) const { visitor->VisitAddExpr(*this); }
+};
+
+// A subtraction expression.
+class SubtractExpr : public BinaryOperatorExpr {
+ public:
+  SubtractExpr(std::unique_ptr<const AstNode> lhs,
+               std::unique_ptr<const AstNode> rhs)
+      : BinaryOperatorExpr(std::move(lhs), std::move(rhs)) {}
+
+  void Visit(AstVisitor* visitor) const { visitor->VisitSubtractExpr(*this); }
+};
+
+// A multiplication expression.
+class MultiplyExpr : public BinaryOperatorExpr {
+ public:
+  MultiplyExpr(std::unique_ptr<const AstNode> lhs,
+               std::unique_ptr<const AstNode> rhs)
+      : BinaryOperatorExpr(std::move(lhs), std::move(rhs)) {}
+
+  void Visit(AstVisitor* visitor) const { visitor->VisitMultiplyExpr(*this); }
+};
+
+// A division expression.
+class DivideExpr : public BinaryOperatorExpr {
+ public:
+  DivideExpr(std::unique_ptr<const AstNode> lhs,
+             std::unique_ptr<const AstNode> rhs)
+      : BinaryOperatorExpr(std::move(lhs), std::move(rhs)) {}
+
+  void Visit(AstVisitor* visitor) const { visitor->VisitDivideExpr(*this); }
+};
+
+}  // namespace version_1
+}  // namespace abstract_syntax
+}  // namespace cs160
+
+#endif  // ABSTRACT_SYNTAX_ABSTRACT_SYNTAX_TREE_V1_H_
---
+++ abstract_syntax/print_visitor_v1_test.cc
@@ -0,0 +1,76 @@
+#include <sstream>
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "abstract_syntax/abstract_syntax_tree_v1.h"
+#include "abstract_syntax/print_visitor_v1.h"
+#include "utility/memory.h"
+
+using cs160::abstract_syntax::version_1::IntegerExpr;
+using cs160::abstract_syntax::version_1::AddExpr;
+using cs160::abstract_syntax::version_1::SubtractExpr;
+using cs160::abstract_syntax::version_1::MultiplyExpr;
+using cs160::abstract_syntax::version_1::DivideExpr;
+using cs160::abstract_syntax::version_1::PrintVisitor;
+
+using cs160::make_unique;
+
+class PrinterTest : public ::testing::Test {
+ protected:
+  PrintVisitor printer_;
+};
+
+TEST_F(PrinterTest, IntegerExprIsVisited) {
+  auto number = make_unique<IntegerExpr>(7);
+  number->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "7");
+}
+
+TEST_F(PrinterTest, AddExprIsVisited) {
+  auto expr = make_unique<AddExpr>((make_unique<IntegerExpr>(7)),
+                                   make_unique<IntegerExpr>(5));
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(+ 7 5)");
+}
+
+TEST_F(PrinterTest, SubtractExprIsVisited) {
+  auto expr = make_unique<SubtractExpr>(make_unique<IntegerExpr>(7),
+                                        make_unique<IntegerExpr>(5));
+
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(- 7 5)");
+}
+
+TEST_F(PrinterTest, MultiplyExprIsVisited) {
+  auto expr = make_unique<MultiplyExpr>(make_unique<IntegerExpr>(7),
+                                        make_unique<IntegerExpr>(5));
+
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(* 7 5)");
+}
+
+TEST_F(PrinterTest, DivideExprIsVisited) {
+  auto expr = make_unique<DivideExpr>(make_unique<IntegerExpr>(7),
+                                      make_unique<IntegerExpr>(5));
+
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(/ 7 5)");
+}
+
+TEST_F(PrinterTest, NestedVisitationsWorkProperly) {
+  auto expr = make_unique<DivideExpr>(
+      make_unique<AddExpr>(make_unique<IntegerExpr>(7),
+                           make_unique<IntegerExpr>(5)),
+      make_unique<SubtractExpr>(make_unique<IntegerExpr>(2),
+                                make_unique<IntegerExpr>(1)));
+
+  expr->Visit(&printer_);
+
+  EXPECT_EQ(printer_.GetOutput(), "(/ (+ 7 5) (- 2 1))");
+}
