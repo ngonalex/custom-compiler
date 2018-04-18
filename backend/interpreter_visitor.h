a799e664fb1d00fd83ae82f1f9a7dd072dd0d0c6
Alexander Ngo <alexanderngo@umail.ucsb.edu>
2018-04-17 19:24:27 -0700

    Fixed some formatting issues

--- backend/interpreter_visitor.h
+++ backend/interpreter_visitor.h
@@ -34,58 +34,57 @@ class InterpreterVisitor : public AstVisitor{
 
   void VisitAddExpr(const AddExpr& exp) {
     // Visit left then right (eventually reaches the base case of Int
-	  // and that will push two values on the stack)
-	  exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
-   	exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
+    // and that will push two values on the stack)
+    exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
+    exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
 
-	  // Pop the top two (left,right), add them, and push it back on the stack
+    // Pop the top two (left,right), add them, and push it back on the stack
     int r = opstack_.top();
     opstack_.pop();
     int l = opstack_.top();
     opstack_.pop();
-	  opstack_.push(l+r);
+    opstack_.push(l+r);
   }
 
   void VisitSubtractExpr(const SubtractExpr& exp) {
-   	// Visit left then right(eventually reaches the base case of Int)
-	  // and that will push the two values on the stack 
-	  exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
-	  exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
+    // Visit left then right(eventually reaches the base case of Int)
+    // and that will push the two values on the stack 
+    exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
+    exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
 
-	  // Pop (left,right), subtract them, and push it back onto the stack
-	  int r = opstack_.top();
+    // Pop (left,right), subtract them, and push it back onto the stack
+    int r = opstack_.top();
     opstack_.pop();
     int l = opstack_.top();
     opstack_.pop();
-	  opstack_.push(l-r);
-  
+    opstack_.push(l-r);
   }
 
   void VisitMultiplyExpr(const MultiplyExpr& exp) {
-   	// Visit lhs,rhs
-	  exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
-	  exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
+    // Visit lhs,rhs
+    exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
+    exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
 
-	  // Pop top two, push result back in
-	  int r = opstack_.top();
+    // Pop top two, push result back in
+    int r = opstack_.top();
     opstack_.pop();
     int l = opstack_.top();
     opstack_.pop();
-	  opstack_.push(l*r);
+    opstack_.push(l*r);
   }
 
   void VisitDivideExpr(const DivideExpr& exp) {
-	  // Visit left/right
-	  exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
-	  exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
+    // Visit left/right
+    exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
+    exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
 
-	  // Pop top two, push result back in
-	  // Should we check for division by zero here?
-	  int r = opstack_.top();
+    // Pop top two, push result back in
+    // Should we check for division by zero here?
+    int r = opstack_.top();
     opstack_.pop();
     int l = opstack_.top();
     opstack_.pop();
-	  opstack_.push(l/r);
+    opstack_.push(l/r);
   }
 
   private:
