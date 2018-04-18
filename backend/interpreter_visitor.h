45ec4053b574360b72b24d165abf535223fca82d
Alex Ngo <alexanderngo@umail.ucsb.edu>
2018-04-17 17:36:14 -0700

    moved lowerer and interpreter to backend

---
+++ backend/interpreter_visitor.h
@@ -0,0 +1,100 @@
+#ifndef BACKEND_INTERPRETER_VISITOR_H_
+#define BACKEND_INTERPRETER_VISITOR_H_
+
+#include <stack>
+
+#include "abstract_syntax/abstract_syntax_tree.h"
+
+using cs160::abstract_syntax::AstVisitor;
+using cs160::abstract_syntax::IntegerExpr;
+using cs160::abstract_syntax::AddExpr;
+using cs160::abstract_syntax::SubtractExpr;
+using cs160::abstract_syntax::MultiplyExpr;
+using cs160::abstract_syntax::DivideExpr;
+using cs160::abstract_syntax::BinaryOperatorExpr;
+
+namespace cs160{
+namespace backend{
+
+class InterpreterVisitor : public AstVisitor{
+ public:
+  InterpreterVisitor() {}
+  ~InterpreterVisitor() {}
+
+  const int GetOutput() const { return opstack_.top(); }
+
+  // these should be able to change members of the visitor, thus not const
+  void VisitIntegerExpr(const IntegerExpr& exp){
+    //push value to stack
+    opstack_.push(exp.value());
+  } 
+
+  void VisitBinaryOperatorExpr(const BinaryOperatorExpr& exp){
+  }
+
+  void VisitAddExpr(const AddExpr& exp) {
+    //Visit left then right (eventually reaches the base case of Int
+	  //and that will push two values on the stack)
+	  exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
+   	exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
+
+	  //Pop the top two (left,right), add them, and push it back on the stack
+    int r = opstack_.top();
+    opstack_.pop();
+    int l = opstack_.top();
+    opstack_.pop();
+	  opstack_.push(l+r);
+  }
+
+  void VisitSubtractExpr(const SubtractExpr& exp) {
+   	//Visit left then right(eventually reaches the base case of Int)
+	  //and that will push the two values on the stack 
+	  exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
+	  exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
+
+	  //Pop (left,right), subtract them, and push it back onto the stack
+	  int r = opstack_.top();
+    opstack_.pop();
+    int l = opstack_.top();
+    opstack_.pop();
+	  opstack_.push(l-r);
+  
+  }
+
+  void VisitMultiplyExpr(const MultiplyExpr& exp) {
+   	//Visit lhs,rhs
+	  exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
+	  exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
+
+	  //Pop top two, push result back in
+	  int r = opstack_.top();
+    opstack_.pop();
+    int l = opstack_.top();
+    opstack_.pop();
+	  opstack_.push(l*r);
+  }
+
+  void VisitDivideExpr(const DivideExpr& exp) {
+	  //Visit left/right
+	  exp.lhs().Visit(const_cast<InterpreterVisitor*>(this));
+	  exp.rhs().Visit(const_cast<InterpreterVisitor*>(this));
+
+	  //Pop top two, push result back in
+	  //Should we check for division by zero here?
+	  int r = opstack_.top();
+    opstack_.pop();
+    int l = opstack_.top();
+    opstack_.pop();
+	  opstack_.push(l/r);
+  }
+
+  private:
+    //Not very general, this is probably a bad idea for future ASTS
+    std::stack<int> opstack_;
+
+};
+
+}//namespace backend
+}//namespace cs160
+
+#endif //BACKEND_INTERPRETER_VISITOR_H_
---
+++ backend/lowerer_visitor_test.cc
@@ -0,0 +1,97 @@
+
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "abstract_syntax/abstract_syntax_tree.h"
+#include "backend/lowerer_visitor.h"
+#include "utility/memory.h"
+
+
+using cs160::abstract_syntax::IntegerExpr;
+using cs160::abstract_syntax::AddExpr;
+using cs160::abstract_syntax::SubtractExpr;
+using cs160::abstract_syntax::MultiplyExpr;
+using cs160::abstract_syntax::DivideExpr;
+using cs160::backend::LowererVisitor;
+using cs160::abstract_syntax::AstVisitor;
+
+using cs160::make_unique;
+
+class LowererTest : public ::testing::Test {
+ protected:
+  LowererVisitor lowerer_;
+};
+
+TEST_F(LowererTest, IntegerExprIsVisited) {
+  auto number = make_unique<IntegerExpr>(7);
+  number->Visit(&lowerer_);
+
+  EXPECT_EQ(lowerer_.GetOutput(), "t_0 <- 7\n");
+}
+
+TEST_F(LowererTest, AddExprIsVisited) {
+  auto expr = cs160::make_unique<AddExpr>((make_unique<IntegerExpr>(7)),
+                                   make_unique<IntegerExpr>(5));
+  expr->Visit(&lowerer_);
+
+  //t_0 <- 7
+  //t_1 <- 5
+  //t_2 <- t_0 + t_1
+  EXPECT_EQ(lowerer_.GetOutput(), "t_0 <- 7\nt_1 <- 5\nt_2 <- t_0 + t_1\n");
+}
+
+//t_0 <- 7
+//t_1 <- 5
+//t_2 <- t_0 - t_1
+TEST_F(LowererTest, SubtractExprIsVisited) {
+  auto expr = cs160::make_unique<SubtractExpr>(make_unique<IntegerExpr>(7),
+                                        make_unique<IntegerExpr>(5));
+
+  expr->Visit(&lowerer_);
+
+  EXPECT_EQ(lowerer_.GetOutput(), "t_0 <- 7\nt_1 <- 5\nt_2 <- t_0 - t_1\n");
+}
+
+TEST_F(LowererTest, MultiplyExprIsVisited) {
+  auto expr = cs160::make_unique<MultiplyExpr>(make_unique<IntegerExpr>(7),
+                                        make_unique<IntegerExpr>(5));
+
+  expr->Visit(&lowerer_);
+
+  //t_0 <- 7
+  //t_1 <- 5
+  //t_2 <- t_0 * t_1
+  EXPECT_EQ(lowerer_.GetOutput(), "t_0 <- 7\nt_1 <- 5\nt_2 <- t_0 * t_1\n");
+}
+
+TEST_F(LowererTest, DivideExprIsVisited) {
+  auto expr = cs160::make_unique<DivideExpr>(make_unique<IntegerExpr>(7),
+                                      make_unique<IntegerExpr>(5));
+
+  expr->Visit(&lowerer_);
+  
+  //t_0 <- 7
+  //t_1 <- 5
+  //t_2 <- t_0 / t_1
+  EXPECT_EQ(lowerer_.GetOutput(), "t_0 <- 7\nt_1 <- 5\nt_2 <- t_0 / t_1\n");
+}
+
+TEST_F(LowererTest, NestedVisitationsWorkProperly) {
+  auto expr = cs160::make_unique<DivideExpr>(
+      cs160::make_unique<AddExpr>(make_unique<IntegerExpr>(7),
+                           make_unique<IntegerExpr>(5)),
+      cs160::make_unique<SubtractExpr>(make_unique<IntegerExpr>(2),
+                                make_unique<IntegerExpr>(1)));
+
+  expr->Visit(&lowerer_);
+
+  //t_0 <- 7
+  //t_1 <- 5
+  //t_2 <- t_0 + t_1
+  //t_3 <- 2
+  //t_4 <- 1
+  //t_5 <- t_3 - t_4
+  //t_6 <- t_2 / t_5
+  EXPECT_EQ(lowerer_.GetOutput(), "t_0 <- 7\nt_1 <- 5\nt_2 <- t_0 + t_1\nt_3 <- 2\nt_4 <- 1\nt_5 <- t_3 - t_4\nt_6 <- t_2 / t_5\n");
+}
