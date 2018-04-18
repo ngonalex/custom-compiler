f9a9d643f63e3e10833f12db90270410771f2672
Chandler <chan4est@gmail.com>
2018-04-17 21:49:49 -0700

    Super basic operators

---
+++ frontend/parser/parser.cc
@@ -0,0 +1,70 @@
+#include "./parser.h"
+
+namespace cs160 {
+namespace frontend {
+  
+  Parser::Parser(std::vector<Token> program) {
+    this->program = program;
+    this->result = new ParseResult;
+  }
+  
+  // Ecounter parenthesis and numbers
+  void Parser::start() {
+    ParseResult* current = new ParseResult;
+    if (program[0].getType()  == Type::NUM) {
+      program[0].print();
+      this->result = parseOperator(current, 1);
+      printf("Result: %i \n", this->result->left.getVal() * this->result->right.getVal());
+    }
+  }
+  
+  ParseResult* Parser::parseOperator(ParseResult* p, int location) {
+    switch(program[location].getType()) {
+      case Type::ADD_OP:
+        program[location].print();
+        return (parseInt(p, ++location));
+        break;
+      case Type::SUB_OP:
+        program[location].print();
+        return (parseInt(p, ++location));
+        break;
+      case Type::MUL_OP:
+        program[location].print();
+        return (parseInt(p, ++location));
+        break;
+      case Type::DIV_OP:
+        program[location].print();
+        return (parseInt(p, ++location));
+        break;
+    }
+  }
+  
+  ParseResult* Parser::parseInt(ParseResult* p, int location) {
+    if (program[location].getType() == Type::NUM) {
+      program[location].print();
+      p->left = program[0];
+      p->op = program[1];
+      p->right = program[2];
+      return p;
+    }
+  }
+  
+  
+  
+} // namespace frontend
+} // namespace cs160
+
+int main() {
+  cs160::frontend::Token firstToken(cs160::frontend::Type::NUM, 6);
+  cs160::frontend::Token secondToken(cs160::frontend::Type::MUL_OP);
+  cs160::frontend::Token thirdToken(cs160::frontend::Type::NUM, 3);
+  std::vector<cs160::frontend::Token> test_vector;
+  test_vector.push_back(firstToken);
+  test_vector.push_back(secondToken);
+  test_vector.push_back(thirdToken);
+  
+  cs160::frontend::Parser parser(test_vector);
+  parser.start();
+  
+  return 0;
+}
---
+++ frontend/parser/parser.h
@@ -0,0 +1,55 @@
+#ifndef PARSER_PARSER_H_
+#define PARSER_PARSER_H_
+
+#include <vector>
+#include "../tokenizer/tokenizer.h"
+
+namespace cs160 {
+namespace frontend {
+  // 3 + 5 -- sure
+  // 3 * 5 + 8
+  // /0 = 
+  /*
+        +
+    *        8
+  3   5  
+  
+    E -> E + T | E - T | T
+    T -> T * F | T / F | F
+    F -> ( E ) | Num
+    
+    ParseResult parseInt(ParseResult p, int current);
+    ParseResult matchChar(ParseResult, char current);
+    
+    ParseResult expect(ParseResult p) {
+      if(!p.isSuccess()) {
+        throw 
+      }
+    }
+      
+  */
+  
+  class ParseResult {
+    public:
+      Token left;
+      Token op;
+      Token right;
+  };
+    
+  class Parser {
+    public:
+      Parser(std::vector<Token> program);
+      ParseResult* parseInt(ParseResult *p, int location);
+      ParseResult* parseOperator(ParseResult *p, int location);
+      void start();
+    
+    private:
+      ParseResult *result;
+      std::vector<Token> program;
+  };
+  
+  
+} // namespace frontend
+} // namespace cs160
+
+#endif // PARSER_PARSER_H_
