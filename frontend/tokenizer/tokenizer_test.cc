cbbc36c6fc7fdfa6d57ed998138de3b8fb1fc1b9
Alexander Ngo <alexanderngo@umail.ucsb.edu>
2018-04-19 13:19:36 -0700

    Update tokenizer_test.cc
    Added two more tests, checking if it handles weird number of spaces correctly and if it handles unexpected tokens correctly.

--- frontend/tokenizer/tokenizer_test.cc
+++ frontend/tokenizer/tokenizer_test.cc
@@ -53,3 +53,33 @@ TEST(Tokenizer, complexExpression) {
     i++;
   }
 }
+
+TEST(Tokenizer, WeirdSpacing) {
+  Tokenizer lexer("  (  3  +  3  )   *  4/2   -1 ");
+  std::vector<Token> tokens;
+  tokens.push_back(Token(OPEN_PAREN));
+  tokens.push_back(Token(NUM, 3));
+  tokens.push_back(Token(ADD_OP));
+  tokens.push_back(Token(NUM, 3));
+  tokens.push_back(Token(CLOSE_PAREN));
+  tokens.push_back(Token(MUL_OP));
+  tokens.push_back(Token(NUM, 4));
+  tokens.push_back(Token(DIV_OP));
+  tokens.push_back(Token(NUM, 2));
+  tokens.push_back(Token(SUB_OP));
+  tokens.push_back(Token(NUM, 1));
+
+  int i = 0;
+  for (Token a : lexer.tokens()) {
+    EXPECT_EQ(a, tokens[i]);
+    i++;
+  }
+}
+
+TEST(Tokenizer, UnexpectedToken) {
+  Tokenizer lexer("5 + x");
+  std::vector<Token> tokens;
+  tokens.push_back(Token(FAILED));
+  
+  EXPECT_EQ(lexer.tokens()[0],tokens[0]);
+}
