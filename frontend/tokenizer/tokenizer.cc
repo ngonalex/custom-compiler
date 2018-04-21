8f2493d4e4178ea920f6f64350047af3ec228980
victorcheng7 <victorcheng@ResNet-51-12.resnet.ucsb.edu>
2018-04-20 21:52:51 -0700

    abstracted .cc files, compilation errors not finished

---
+++ frontend/tokenizer/tokenizer.cc
@@ -0,0 +1,65 @@
+#include "frontend/tokenizer/tokenizer.h"
+
+using namespace cs160::frontend;
+
+explicit Tokenizer::Tokenizer(std::string program) : input_program_(program) {
+  char prevChar;
+  Token::Type prevType = Token::NONE;
+  std::string currString;
+
+  for (char &c : input_program_) {
+    if (c == ' ')
+      continue;
+    Token::Type type = ExtractType(c);
+    if (type != Token::FAILED) {
+      std::stringstream currStringStream;
+      currStringStream << currString;
+      currStringStream << c;
+
+      currString = currStringStream.str();
+      if (type != prevType) {
+        if (type == Token::NUM) {
+          Token newToken(type, atoi(currString.c_str()));
+          tokens_.push_back(newToken);
+        } else {
+          Token newToken(type);
+          tokens_.push_back(newToken);
+        }
+        currString = "";
+      }
+      prevChar = c;
+      prevType = type;
+    } else {
+      Token newToken(Token::FAILED);
+      tokens_[0] = (newToken);
+      break;
+      // Return just one token that is null
+    }
+  }
+}
+
+Token::Type Tokenizer::ExtractType(const char testChar) {
+  if (isdigit(testChar))
+    return Token::NUM;
+  switch (testChar) {
+  case '(': {
+    return Token::OPEN_PAREN;
+  }
+  case ')': {
+    return Token::CLOSE_PAREN;
+  }
+  case '+': {
+    return Token::ADD_OP;
+  }
+  case '-': {
+    return Token::SUB_OP;
+  }
+  case '*': {
+    return Token::MUL_OP;
+  }
+  case '/': {
+    return Token::DIV_OP;
+  }
+  default: { return Token::FAILED; }
+  }
+}
