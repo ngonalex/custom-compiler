4168a69008617799783e932f863944f264fdf2a8
michaelamalfitano <amalfitano@umail.ucsb.edu>
2018-04-24 22:08:59 -0700

    Separated parser.h functions into new parser.cc file

--- frontend/parser/parser.h
+++ frontend/parser/parser.h
@@ -24,100 +24,39 @@ class Parser {
     ASSERT(program.size() != 0, "Program cannot be empty tokens");
     std::reverse(this->program_.begin(), this->program_.end());
   }
+
   Parser(){ }
 
   /** Subroutines **/
   // Returns the type of the token in the 'front' of the program_
   Token::Type Next() { return program_.back().type(); }
+
   // Removes a token from the program_ vector
   void Consume() { program_.pop_back(); };
+
   // Prints an error message with information of current token
   void Error() {
     std::cerr << "Error!" << std::endl;
     exit(-1);
   }
+
   // Consume Token if proper type, otherwise error
   void Expect(Token::Type type) { Next() == type ? Consume() : Error(); }
 
   // AST Expressions
-  std::unique_ptr<const AstNode> mkNode(Token::Type op, std::unique_ptr<const AstNode> first_leaf, std::unique_ptr<const AstNode> second_leaf) {
-    switch(op) {
-      case Token::Type::ADD_OP: {
-       return make_unique<AddExpr>(std::move(first_leaf), std::move(second_leaf));
-      }
-      case Token::Type::SUB_OP: {
-       return make_unique<SubtractExpr>(std::move(first_leaf), std::move(second_leaf));
-      }
-      case Token::Type::MUL_OP: {
-       return make_unique<MultiplyExpr>(std::move(first_leaf), std::move(second_leaf));
-      }
-      case Token::Type::DIV_OP: {
-       return make_unique<DivideExpr>(std::move(first_leaf), std::move(second_leaf));
-      }
-      default: {
-        Error();
-        return nullptr;
-      }
-    }
-  }
+  std::unique_ptr<const AstNode> mkNode(Token::Type op, 
+    std::unique_ptr<const AstNode> first_leaf,
+    std::unique_ptr<const AstNode> second_leaf);
+    
+  std::unique_ptr<const AstNode> mkLeaf(Token num);
 
-  std::unique_ptr<const AstNode> mkLeaf(Token num) {
-    ASSERT(num.isNumber(), "Error creating IntegerExpr");
-    return make_unique<IntegerExpr>(num.val());
-  }
+  std::unique_ptr<const AstNode> Eparser();
 
-  // every 't' is supposed to be a Tree
-  std::unique_ptr<const AstNode> Eparser() {
-    std::unique_ptr<const AstNode> t = ParseAddSub();
-    Expect(Token::Type::END);
-    return t;
-  }
+  std::unique_ptr<const AstNode> ParseAddSub();
 
-  std::unique_ptr<const AstNode> ParseAddSub() {
-    std::unique_ptr<const AstNode> t = ParseMulDiv();
-    Token::Type op = Next();
-    while (op == Token::Type::ADD_OP || op == Token::Type::SUB_OP) {
-      Consume();
-      std::unique_ptr<const AstNode> t1 = ParseMulDiv();
-      t = mkNode(op, std::move(t), std::move(t1));
-      op = Next();
-    }
-    return t;
-  }
+  std::unique_ptr<const AstNode> ParseMulDiv();
 
-  std::unique_ptr<const AstNode> ParseMulDiv() {
-    std::unique_ptr<const AstNode> t = ParseExpression();
-    Token::Type op = Next();
-    while (op == Token::Type::MUL_OP || op == Token::Type::DIV_OP) {
-      Consume();
-      std::unique_ptr<const AstNode> t1 = ParseExpression();
-      t = mkNode(op, std::move(t), std::move(t1));
-      op = Next();
-    }
-    return t;
-  }
-
-  std::unique_ptr<const AstNode> ParseExpression() {
-    // Either returns an Int
-      Token::Type type = Next();
-    if (type == Token::Type::NUM) {
-      std::unique_ptr<const AstNode> t = mkLeaf(program_.back());
-      Consume();
-      return t;
-    }
-    // An Expression
-    else if (type == Token::Type::OPEN_PAREN) {
-      Consume();
-      std::unique_ptr<const AstNode> t = ParseAddSub();
-      Expect(Token::Type::CLOSE_PAREN);
-      return t;
-    }
-    // Or an error
-    else {
-      Error();
-      return NULL;
-    }
-  }
+  std::unique_ptr<const AstNode> ParseExpression();
 
 // Tokens will now be in reverse order!
  private:
