99a00ea2f459441def25e54b09f6a02d96aaee08
Chandler <chan4est@gmail.com>
2018-05-07 15:14:04 -0700

    Before assignments w/o var

--- frontend/parser/parser_test_v2.cc
+++ frontend/parser/parser_test_v2.cc
@@ -39,7 +39,7 @@ TEST(Parser, Assignment) {
   result->Visit(a);
   std::string output = a->GetOutput();
   // 
-  EXPECT_EQ(output, "(= 5); x");
+  EXPECT_EQ(output, "x = 5; x");
 }
 
 // var x : int = 6;
@@ -88,5 +88,50 @@ TEST(Parser, SimpleProgram) {
   result->Visit(a);
   std::string output = a->GetOutput();
   // 
-  EXPECT_EQ(output, "(= 6); (= 7); (+ x y)");
+  EXPECT_EQ(output, "x = 6; y = 7; (+ x y)");
+}
+
+// var x : int = -10;
+// x;
+TEST(Parser, NegativeNums) {
+  Token var(Token::Type::VAR_NAME);
+  Token x(Token::Type::IDENTIFIER, "x");
+  Token y(Token::Type::IDENTIFIER, "y");
+  Token add(Token::Type::ADD_OP);
+  Token field(Token::Type::FIELD);
+  Token int_type(Token::Type::IDENTIFIER, "int");
+  Token equals(Token::Type::EQUAL_SIGN);
+  Token neg10(Token::Type::NUM, -10);
+  Token seven(Token::Type::NUM, 7);
+  Token end(Token::Type::END);
+  Token endfile(Token::Type::ENDOFFILE);  
+  std::vector<Token> test_vector;
+  // var x : int = -10;
+  test_vector.push_back(var);
+  test_vector.push_back(x);
+  test_vector.push_back(field);
+  test_vector.push_back(int_type);
+  test_vector.push_back(equals);
+  test_vector.push_back(neg10);
+  test_vector.push_back(end);
+  // x
+  test_vector.push_back(x);
+  test_vector.push_back(end);
+  test_vector.push_back(endfile);
+  Parser parser(test_vector);
+  std::unique_ptr<const AstNode> result = parser.ParseProgram();
+  // Read output
+  PrintVisitor *a = new PrintVisitor();
+  result->Visit(a);
+  std::string output = a->GetOutput();
+  // 
+  EXPECT_EQ(output, "x = -10; x");
+}
+
+// var x : int = -99;
+// var y : int = 110101;
+// var z : int = 0;
+// z = x + y;
+TEST(Parser, Assignment) {
+  
 }
