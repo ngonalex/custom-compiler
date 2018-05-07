5eb521c661c31da22aa397c304f990d7a0ca2f21
Chandler <chan4est@gmail.com>
2018-05-07 14:57:37 -0700

    More simple tests

--- frontend/parser/parser_test_v2.cc
+++ frontend/parser/parser_test_v2.cc
@@ -35,9 +35,58 @@ TEST(Parser, Assignment) {
   Parser parser(test_vector);
   std::unique_ptr<const AstNode> result = parser.ParseProgram();
   // Read output
-  // PrintVisitor *a = new PrintVisitor();
-  // result->Visit(a);
-  // std::string output = a->GetOutput();
+  PrintVisitor *a = new PrintVisitor();
+  result->Visit(a);
+  std::string output = a->GetOutput();
   // 
-  // EXPECT_EQ(output, "5");
+  EXPECT_EQ(output, "(= 5); x");
+}
+
+// var x : int = 6;
+// var y : int = 7;
+// x + y;
+TEST(Parser, SimpleProgram) {
+  Token var(Token::Type::VAR_NAME);
+  Token x(Token::Type::IDENTIFIER, "x");
+  Token y(Token::Type::IDENTIFIER, "y");
+  Token add(Token::Type::ADD_OP);
+  Token field(Token::Type::FIELD);
+  Token int_type(Token::Type::IDENTIFIER, "int");
+  Token equals(Token::Type::EQUAL_SIGN);
+  Token six(Token::Type::NUM, 6);
+  Token seven(Token::Type::NUM, 7);
+  Token end(Token::Type::END);
+  Token endfile(Token::Type::ENDOFFILE);  
+  std::vector<Token> test_vector;
+  // var x : int = 6;
+  test_vector.push_back(var);
+  test_vector.push_back(x);
+  test_vector.push_back(field);
+  test_vector.push_back(int_type);
+  test_vector.push_back(equals);
+  test_vector.push_back(six);
+  test_vector.push_back(end);
+  // var y : int = 7;
+  test_vector.push_back(var);
+  test_vector.push_back(y);
+  test_vector.push_back(field);
+  test_vector.push_back(int_type);
+  test_vector.push_back(equals);
+  test_vector.push_back(seven);
+  test_vector.push_back(end);
+  // x + y
+  test_vector.push_back(x);
+  test_vector.push_back(add);
+  test_vector.push_back(y);
+  test_vector.push_back(end);
+  
+  test_vector.push_back(endfile);
+  Parser parser(test_vector);
+  std::unique_ptr<const AstNode> result = parser.ParseProgram();
+  // Read output
+  PrintVisitor *a = new PrintVisitor();
+  result->Visit(a);
+  std::string output = a->GetOutput();
+  // 
+  EXPECT_EQ(output, "(= 6); (= 7); (+ x y)");
 }
