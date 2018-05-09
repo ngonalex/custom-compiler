a441126da8616ef904034f0d96b8ef2e89479970
victorcheng7 <victorcheng@169-231-166-14.wireless.ucsb.edu>
2018-05-09 15:15:10 -0700

    modified some parsestatus

--- frontend/combinators/parsestatus.h
+++ frontend/combinators/parsestatus.h
@@ -1,8 +1,14 @@
 #include <string>
 
-class ParseStatus {
+class ParseStatus { // Super class
 	bool status;
 	std::string remainingCharacters;
+	std::string parsedCharacters;
+
+	// Only for the success case
+
+	// Only for the failed case
+	std::string expectedCharacters;
 
 	// check if two ParseStatuses are equal
 	bool operator==(const ParseStatus &b) const;
