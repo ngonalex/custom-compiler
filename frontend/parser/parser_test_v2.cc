fd1a7235c85f508b4fda25bca5186338358a9c7c
Chandler <chan4est@gmail.com>
2018-05-08 17:32:51 -0700

    Finished v2

--- frontend/parser/parser_test_v2.cc
+++ frontend/parser/parser_test_v2.cc
@@ -188,42 +188,42 @@ TEST(parser_v2_success, RealNegationWithParen) {
 
 // x = 0 + 0;
 // x;
-// TEST(Parser, AltAssignment) {
-//   Token var(Token::Type::VAR_NAME);
-//   Token x(Token::Type::IDENTIFIER, "x");
-//   Token y(Token::Type::IDENTIFIER, "y");
-//   Token z(Token::Type::IDENTIFIER, "z");
-//   Token field(Token::Type::FIELD);
-//   Token int_type(Token::Type::IDENTIFIER, "int");   
-//   Token equals(Token::Type::EQUAL_SIGN);
-//   Token zero(Token::Type::NUM, 0);   
-//   Token add(Token::Type::ADD_OP);
-//   Token end(Token::Type::END);
-//   Token endfile(Token::Type::ENDOFFILE); 
-//   std::vector<Token> test_vector;
-// 
-//   // x = -9 + 0;
-//   test_vector.push_back(x);
-//   test_vector.push_back(equals);
-//   test_vector.push_back(zero);
-//   test_vector.push_back(add);
-//   test_vector.push_back(zero);
-//   test_vector.push_back(end);
-// 
-//   // x;
-//   test_vector.push_back(x);
-//   test_vector.push_back(end);
-//   test_vector.push_back(endfile);
-// 
-//   Parser parser(test_vector);
-//   std::unique_ptr<const AstNode> result = parser.ParseProgram();
-//   // Read output
-//   PrintVisitor *a = new PrintVisitor();
-//   result->Visit(a);
-//   std::string output = a->GetOutput();
-//   // 
-//   EXPECT_EQ(output, "x = -9 + 0; x");
-// }
+TEST(Parser, AltAssignment) {
+  Token var(Token::Type::VAR_NAME);
+  Token x(Token::Type::IDENTIFIER, "x");
+  Token y(Token::Type::IDENTIFIER, "y");
+  Token z(Token::Type::IDENTIFIER, "z");
+  Token field(Token::Type::FIELD);
+  Token int_type(Token::Type::IDENTIFIER, "int");   
+  Token equals(Token::Type::EQUAL_SIGN);
+  Token zero(Token::Type::NUM, 0);   
+  Token add(Token::Type::ADD_OP);
+  Token end(Token::Type::END);
+  Token endfile(Token::Type::ENDOFFILE); 
+  std::vector<Token> test_vector;
+
+  // x = 0 + 0;
+  test_vector.push_back(x);
+  test_vector.push_back(equals);
+  test_vector.push_back(zero);
+  test_vector.push_back(add);
+  test_vector.push_back(zero);
+  test_vector.push_back(end);
+
+  // x;
+  test_vector.push_back(x);
+  test_vector.push_back(end);
+  test_vector.push_back(endfile);
+
+  Parser parser(test_vector);
+  std::unique_ptr<const AstNode> result = parser.ParseProgram();
+  // Read output
+  PrintVisitor *a = new PrintVisitor();
+  result->Visit(a);
+  std::string output = a->GetOutput();
+  // 
+  EXPECT_EQ(output, "x = (0 + 0); x");
+}
 
 
 // var x : int = 99;
@@ -238,72 +238,72 @@ TEST(parser_v2_success, RealNegationWithParen) {
 // VAR X : INT = SUB 99;
 // SUB 99
 // 99 SUB 99
-// TEST(Parser, Comp) {
-//   Token var(Token::Type::VAR_NAME);
-//   Token x(Token::Type::IDENTIFIER, "x");
-//   Token y(Token::Type::IDENTIFIER, "y");
-//   Token z(Token::Type::IDENTIFIER, "z");
-//   Token field(Token::Type::FIELD);
-//   Token int_type(Token::Type::IDENTIFIER, "int");   
-//   Token equals(Token::Type::EQUAL_SIGN);
-//   Token neg9(Token::Type::NUM, -99);
-//   Token huge(Token::Type::NUM, 110101);
-//   Token zero(Token::Type::NUM, 0);   
-//   Token add(Token::Type::ADD_OP);
-//   Token end(Token::Type::END);
-//   Token endfile(Token::Type::ENDOFFILE); 
-//   std::vector<Token> test_vector;
-// 
-//   // var x : int = -99;
-//   test_vector.push_back(var);
-//   test_vector.push_back(x);
-//   test_vector.push_back(field);
-//   test_vector.push_back(int_type);
-//   test_vector.push_back(equals);
-//   test_vector.push_back(neg9);
-//   test_vector.push_back(end);
-// 
-//   // var y : int = 110101;
-//   test_vector.push_back(var);
-//   test_vector.push_back(y);
-//   test_vector.push_back(field);
-//   test_vector.push_back(int_type); 
-//   test_vector.push_back(equals);
-//   test_vector.push_back(huge);
-//   test_vector.push_back(end);
-// 
-//   // var z : int = 0;
-//   test_vector.push_back(var);
-//   test_vector.push_back(z);
-//   test_vector.push_back(field);
-//   test_vector.push_back(int_type);
-//   test_vector.push_back(equals);
-//   test_vector.push_back(zero);
-//   test_vector.push_back(end);  
-// 
-//   // z = x + y;
-//   test_vector.push_back(z);
-//   test_vector.push_back(equals);
-//   test_vector.push_back(x);
-//   test_vector.push_back(add);
-//   test_vector.push_back(y);
-//   test_vector.push_back(end);  
-// 
-//   // z;
-//   test_vector.push_back(z);
-//   test_vector.push_back(end); 
-// 
-//   // EOF
-//   test_vector.push_back(endfile);
-// 
-//   Parser parser(test_vector);
-//   std::unique_ptr<const AstNode> result = parser.ParseProgram();
-//   // Read output
-//   PrintVisitor *a = new PrintVisitor();
-//   result->Visit(a);
-//   std::string output = a->GetOutput();
-//   // 
-//   EXPECT_EQ(output, "x = -10; x");
-// }
-// 
-// 
+TEST(Parser, Comp) {
+  Token var(Token::Type::VAR_NAME);
+  Token x(Token::Type::IDENTIFIER, "x");
+  Token y(Token::Type::IDENTIFIER, "y");
+  Token z(Token::Type::IDENTIFIER, "z");
+  Token field(Token::Type::FIELD);
+  Token int_type(Token::Type::IDENTIFIER, "int");   
+  Token equals(Token::Type::EQUAL_SIGN);
+  Token neg9(Token::Type::NUM, -99);
+  Token huge(Token::Type::NUM, 110101);
+  Token zero(Token::Type::NUM, 0);   
+  Token add(Token::Type::ADD_OP);
+  Token end(Token::Type::END);
+  Token endfile(Token::Type::ENDOFFILE); 
+  std::vector<Token> test_vector;
+
+  // var x : int = -99;
+  test_vector.push_back(var);
+  test_vector.push_back(x);
+  test_vector.push_back(field);
+  test_vector.push_back(int_type);
+  test_vector.push_back(equals);
+  test_vector.push_back(neg9);
+  test_vector.push_back(end);
+
+  // var y : int = 110101;
+  test_vector.push_back(var);
+  test_vector.push_back(y);
+  test_vector.push_back(field);
+  test_vector.push_back(int_type); 
+  test_vector.push_back(equals);
+  test_vector.push_back(huge);
+  test_vector.push_back(end);
+
+  // var z : int = 0;
+  test_vector.push_back(var);
+  test_vector.push_back(z);
+  test_vector.push_back(field);
+  test_vector.push_back(int_type);
+  test_vector.push_back(equals);
+  test_vector.push_back(zero);
+  test_vector.push_back(end);  
+
+  // z = x + y;
+  test_vector.push_back(z);
+  test_vector.push_back(equals);
+  test_vector.push_back(x);
+  test_vector.push_back(add);
+  test_vector.push_back(y);
+  test_vector.push_back(end);  
+
+  // z;
+  test_vector.push_back(z);
+  test_vector.push_back(end); 
+
+  // EOF
+  test_vector.push_back(endfile);
+
+  Parser parser(test_vector);
+  std::unique_ptr<const AstNode> result = parser.ParseProgram();
+  // Read output
+  PrintVisitor *a = new PrintVisitor();
+  result->Visit(a);
+  std::string output = a->GetOutput();
+  // 
+  EXPECT_EQ(output, "x = -99; y = 110101; z = 0; z = (x + y); z");
+}
+
+
