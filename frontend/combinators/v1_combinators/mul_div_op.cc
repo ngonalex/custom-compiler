afd080d710276f9df1a63636c6d4516019704528
Chandler <chan4est@gmail.com>
2018-05-13 20:37:24 -0700

    mul_div_expr | term_expr | TODO: Make num leafs and ensure results have proper ASTs

--- frontend/combinators/v1_combinators/mul_div_op.cc
+++ frontend/combinators/v1_combinators/mul_div_op.cc
@@ -1,19 +1,30 @@
 #include "frontend/combinators/v1_combinators/mul_div_op.h"
-#include <string>
 
 #define super NullParser
 
-ParseStatus MulDivOpParser::parse(std::string inputProgram){
+ParseStatus MulDivOpParser::parse(std::string inputProgram) {
 	if (inputProgram.size() == 0){
 		return super::parse(inputProgram);
 	}
-	ParseStatus status;
-
-	if (inputProgram[0] == '*' || inputProgram[0] == '/') {
-		status.status = true;
-		status.remainingCharacters = inputProgram.erase(0, 1);
-	} else {
-		return super::parse(inputProgram);
-	}
-	return status;
+	ParseResult result;
+  
+  // ae
+  TermExpr lhs;
+  lhs.parse(inputProgram);
+  // Append to result's AST here
+  
+  // op
+  MulDivOpParser op;
+  op.parse(lhs.result.remainingCharacters);
+    
+  // ae
+  // Loop through until you're not hitting any more operators
+  while (op.result.status) {
+    TermExpr rhs;
+    rhs.parse(op.result.remainingChracters);
+    // Append to result's AST here
+    result = op.parse(rhs.result.remainingChracters);
+  }
+  
+  return result;
 }
--- frontend/combinators/v1_combinators/term_expr.h
+++ frontend/combinators/v1_combinators/term_expr.h
@@ -1,15 +1,16 @@
-#ifndef MUL_DIV_EXPR_H_
-#define MUL_DIV_EXPR_H_
+#ifndef TERM_EXPR_H
+#define TERM_EXPR_H
 
 #include "abstract_syntax/abstract_syntax.h"
 #include "frontend/combinators/basic_combinators/null.h"
 #include "frontend/combinators/v1_combinators/add_sub_op.h"
 #include "frontend/combinators/v1_combinators/num_parser.h"
-#include "frontend/combinators/v1_combinators/term.h"
+#include "frontend/combinators/v1_combinators/add_sub_expr.h"
 
-class MulDivParser : NullParser {
+
+class TermParser : NullParser {
  public:
    virtual ParseStatus parse(std::string inputProgram); 
 };
 
-#endif // MUL_DIV_EXPR_H_
+#endif // TERM_EXPR_H
