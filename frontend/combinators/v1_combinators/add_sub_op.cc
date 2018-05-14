7aecc59d1d8a2682f454de77b92798f3c5bd8b58
michaelamalfitano <amalfitano@umail.ucsb.edu>
2018-05-13 19:05:56 -0700

    Split single_op into add_sub_op and mul_div_op

--- frontend/combinators/v1_combinators/add_sub_op.cc
+++ frontend/combinators/v1_combinators/add_sub_op.cc
@@ -1,16 +1,15 @@
-#include "frontend/combinators/v1_combinators/single_op.h"
+#include "frontend/combinators/v1_combinators/add_sub_op.h"
 #include <string>
 
 #define super NullParser
 
-ParseStatus SingleOperatorParser::parse(std::string inputProgram){
+ParseStatus AddSubOpParser::parse(std::string inputProgram){
 	if (inputProgram.size() == 0){
 		return super::parse(inputProgram);
 	}
 	ParseStatus status;
 
-	if (inputProgram[0] == '+' || inputProgram[0] == '-' ||
-		(inputProgram[0] == '*' || inputProgram[0] == '/')) {
+	if (inputProgram[0] == '+' || inputProgram[0] == '-') {
 		status.status = true;
 		status.remainingCharacters = inputProgram.erase(0, 1);
 	} else {
---
+++ frontend/combinators/v1_combinators/add_sub_op.h
@@ -0,0 +1,13 @@
+#ifndef ADD_SUB_OP_H_
+#define ADD_SUB_OP_H_
+
+#include "abstract_syntax/abstract_syntax.h"
+#include "frontend/combinators/basic_combinators/null.h"
+
+class SingleOperatorParser : NullParser
+{
+public:
+	virtual ParseStatus parse(std::string inputProgram);
+};
+
+#endif // ADD_SUB_OP_H_
