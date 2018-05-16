6237c1979e207f27e83d77142b538cbd05e498c4
CoolStar <coolstarorganization@gmail.com>
2018-05-15 18:31:16 -0700

    Get parsedCharacters from single char parser

--- frontend/combinators/v1_combinators/single_char.cc
+++ frontend/combinators/v1_combinators/single_char.cc
@@ -13,6 +13,7 @@ ParseStatus SingleCharParser::parse(std::string inputProgram) {
 	if ((inputProgram[0] >= 'a' && inputProgram[0] <= 'z') ||
 		(inputProgram[0] >= 'A' && inputProgram[0] <= 'Z')){
 		status.status = true;
+        status.parsedCharacters = inputProgram[0];
 		status.remainingCharacters = inputProgram.erase(0, 1);
 	} else {
 		return super::parse(inputProgram);
