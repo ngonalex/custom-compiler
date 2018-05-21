eaf481b19ca0b67ffb1754caa9c9452c80a21321
CoolStar <coolstarorganization@gmail.com>
2018-05-21 14:51:54 -0700

    Remove tab characters

--- frontend/combinators/preparser.cc
+++ frontend/combinators/preparser.cc
@@ -2,6 +2,7 @@
 #include <regex>
 
 std::string preparse_Program(std::string program){
+	program = std::regex_replace(program, std:;regex("\t"), "");
     program = std::regex_replace(program, std::regex("\\( +"), "(");
     program = std::regex_replace(program, std::regex(" +\\)"), ")");
     program = std::regex_replace(program, std::regex("; +"), ";");
