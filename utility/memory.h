86d4974bf1080f33e45aa5ef76694d21e448da28
victorcheng7 <victorcheng@169-231-165-185.wireless.ucsb.edu>
2018-05-22 18:04:19 -0700

    added sublcass

--- utility/memory.h
+++ utility/memory.h
@@ -13,6 +13,12 @@ std::unique_ptr<T> make_unique(Args&&... args) {
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
+template<class R, class T>
+std::unique_ptr<R> unique_cast(std::unique_ptr<T> &&p) {
+  T* ptr = p.release();
+  return std::unique_ptr<R>(static_cast<R*>(ptr));
+}
+
 }  // cs160
 
 #endif
