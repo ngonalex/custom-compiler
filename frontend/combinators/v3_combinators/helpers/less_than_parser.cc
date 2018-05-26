bef6fe4bd2b87994173508d7945fd9c1d7c4face
CoolStar <coolstarorganization@gmail.com>
2018-05-25 18:57:18 -0700

    Add basic comparison parsers

---
+++ frontend/combinators/v3_combinators/helpers/less_than_parser.cc
@@ -0,0 +1,23 @@
+#include "frontend/combinators/v3_combinators/helpers/less_than_parser.h"
+#include <string>
+
+#define super NullParser
+
+using namespace cs160::frontend;
+
+ParseStatus LessThanParser::parse(std::string inputProgram) {
+	trim(inputProgram);
+	if (inputProgram.size() == 0) {
+		return super::parse(inputProgram);
+	}
+
+	ParseStatus status;
+
+	if (inputProgram[0] == '<') {
+		status.status = true;
+		status.remainingCharacters = inputProgram.erase(0, 1);
+	} else {
+		return super::parse(inputProgram);
+	}
+	return status;
+}
