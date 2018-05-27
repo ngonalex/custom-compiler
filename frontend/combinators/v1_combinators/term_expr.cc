e3e51cfe074da0268b0bb86e7bb5e0ed66bc27c5
victorcheng7 <victorcheng@ResNet-51-59.resnet.ucsb.edu>
2018-05-27 12:38:16 -0700

    fixed bug regarding not returning proper ast

--- frontend/combinators/v1_combinators/term_expr.cc
+++ frontend/combinators/v1_combinators/term_expr.cc
@@ -36,7 +36,7 @@ ParseStatus TermExprParser::parse(std::string inputProgram, std::string errorTyp
       result.parsedCharacters += aeParseStatus.parsedCharacters;
       result.remainingCharacters = aeParseStatus.remainingCharacters;
       std::unique_ptr<const ArithmeticExpr> zero = make_unique<IntegerExpr>(0);
-      aeParseStatus.ast = std::move(make_unique<const SubtractExpr>(
+      result.ast = std::move(make_unique<const SubtractExpr>(
         unique_cast<const ArithmeticExpr>(std::move(zero)),
         unique_cast<const ArithmeticExpr>(std::move(aeParseStatus.ast))));
     }
@@ -59,22 +59,27 @@ ParseStatus TermExprParser::parse(std::string inputProgram, std::string errorTyp
     if (aeParseStatus.status) {
       result.parsedCharacters += aeParseStatus.parsedCharacters;
       result.remainingCharacters = aeParseStatus.remainingCharacters;
+
       CloseParenParser close_paren;
       ParseStatus cpParseStatus = close_paren.parse(aeParseStatus.remainingCharacters);
       if (cpParseStatus.status) {
         result.parsedCharacters += cpParseStatus.parsedCharacters;
         result.remainingCharacters = cpParseStatus.remainingCharacters;
+        result.ast = std::move(aeParseStatus.ast);
       }
       else {
         result.status = cpParseStatus.status;
+        result.errorType = cpParseStatus.errorType;
       }
     }
     else {
         result.status = aeParseStatus.status;
+        result.errorType = aeParseStatus.errorType;
     }
   }
   else {
     result.status = openParseStatus.status;
+      result.errorType = openParseStatus.errorType;
   }
 
   return result;
