c15b1e84eedc54210bb7447a3b3a658c56f5641c
michaelamalfitano <amalfitano@umail.ucsb.edu>
2018-06-15 16:56:28 -0700

    added v4_test.cc

---
+++ frontend/combinators/v4_combinators/v4_test.cc
@@ -0,0 +1,13 @@
+#include "abstract_syntax/abstract_syntax.h"
+#include "abstract_syntax/print_visitor_v4.h"
+#include "frontend/combinators/v4_combinators/helpers/function_helpers.h"
+#include "frontend/combinators/v4_combinators/main/function_call_parser.h"
+#include "frontend/combinators/v4_combinators/main/function_decl_parser.h"
+
+#include "gtest/gtest.h"
+
+#include <iostream>
+
+using namespace cs160::frontend;
+
+//
