08a4cce0203fd0f3a3ec88581e48a72f458e5b8a
Alexander Ngo <alexanderngo@umail.ucsb.edu>
2018-06-14 23:58:48 -0700

    Build errors fixe

--- abstract_syntax/abstract_syntax.h
+++ abstract_syntax/abstract_syntax.h
@@ -12,7 +12,7 @@
 namespace cs160 {
 namespace abstract_syntax {
 
-namespace frontend = version_1;
+namespace frontend = version_2;
 namespace backend = version_5;
 
 }  // namespace abstract_syntax
