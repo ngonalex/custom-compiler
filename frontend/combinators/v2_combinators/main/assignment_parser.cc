d98a01135b8568a5ef895564b2e319b6b62060d2
CoolStar <coolstarorganization@gmail.com>
2018-06-15 18:51:54 -0700

    Use AST v5

--- frontend/combinators/v2_combinators/main/assignment_parser.cc
+++ frontend/combinators/v2_combinators/main/assignment_parser.cc
@@ -65,7 +65,7 @@ ParseStatus AssignmentParser::do_parse(std::string inputProgram,
         unique_cast<const ArithmeticExpr>(std::move(result.second_ast))));
     }
     else {*/
-    result.ast = std::move(make_unique<const Assignment>(
+    result.ast = std::move(make_unique<const AssignmentFromArithExp>(
         unique_cast<const VariableExpr>(std::move(firstResult.ast)),
         unique_cast<const ArithmeticExpr>(std::move(result.second_ast))));
     //}
