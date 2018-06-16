1c6149980c109b284be9929bd6c98544aaafc773
CoolStar <coolstarorganization@gmail.com>
2018-06-15 22:51:41 -0700

    v5 test (without PrintVisitor currently)

--- frontend/combinators/v1_to_v5_combinators/v5_test.cc
+++ frontend/combinators/v1_to_v5_combinators/v5_test.cc
@@ -3,6 +3,7 @@
 #include "frontend/combinators/v1_to_v5_combinators/helpers/tuple_helpers.h"
 #include "frontend/combinators/v1_to_v5_combinators/main/tuple_creation_parser.h"
 #include "frontend/combinators/v1_to_v5_combinators/main/dereference_parser.h"
+#include "frontend/combinators/v1_to_v5_combinators/main/assignment_parser.h"
 
 #include "gtest/gtest.h"
 
@@ -52,186 +53,57 @@ TEST(FunctionHelpers, FailRightBracket) {
   EXPECT_EQ(result.errorType, "Expecting character: ]");
 }
 
-// TEST(FunctionHelpers, successReturnType) {
-//   ReturnTypeOp parser;
-//   ParseStatus result = parser.do_parse("->", 0);
+TEST(AssignmentParser, assignToArray) {
+  AssignmentParser parser;
+  ParseStatus result = parser.do_parse("z[5] = x;", 0);
 
-//   EXPECT_EQ(result.status, true);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 2);
-//   EXPECT_EQ(result.remainingCharacters, "");
-//   EXPECT_EQ(result.parsedCharacters, "->");
-// }
+  EXPECT_EQ(result.status, true);
+  EXPECT_EQ(result.startCharacter, 0);
+  EXPECT_EQ(result.endCharacter, 9);
+  EXPECT_EQ(result.remainingCharacters, "");
+  EXPECT_EQ(result.parsedCharacters, "z[5]=x;");
+}
 
-// TEST(FunctionHelpers, failReturnType) {
-//   ReturnTypeOp parser;
-//   ParseStatus result = parser.do_parse("not ->", 0);
+TEST(AssignmentParser, assignToArray2) {
+  AssignmentParser parser;
+  ParseStatus result = parser.do_parse("a[ z + 6 + ( 4 + 5 ) ] = x;", 0);
 
-//   EXPECT_EQ(result.status, false);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 0);
-//   EXPECT_EQ(result.errorType, "Expecting character: -");
-// }
+  EXPECT_EQ(result.status, true);
+  EXPECT_EQ(result.startCharacter, 0);
+  EXPECT_EQ(result.endCharacter, 27);
+  EXPECT_EQ(result.remainingCharacters, "");
+  EXPECT_EQ(result.parsedCharacters, "a[z+6+(4+5)]=x;");
+}
 
-// TEST(FunctionHelpers, successFunctionVariable) {
-//   FunctionVariableParsers parser;
-//   ParseStatus result = parser.do_parse("victor : Integer, prabal : String", 0);
+TEST(TupleCreationParser, createTuple) {
+  TupleAssignmentParser parser;
+  ParseStatus result = parser.do_parse("var z : int[5];", 0);
 
-//   EXPECT_EQ(result.status, true);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 33);
-//   EXPECT_EQ(result.remainingCharacters, "");
-//   EXPECT_EQ(result.parsedCharacters, "victor:Integer,prabal:String");
-// }
+  EXPECT_EQ(result.status, true);
+  EXPECT_EQ(result.startCharacter, 0);
+  EXPECT_EQ(result.endCharacter, 15);
+  EXPECT_EQ(result.remainingCharacters, "");
+  EXPECT_EQ(result.parsedCharacters, "varz:int[5];");
+}
 
-// TEST(FunctionHelpers, incompleteFunctionVariable) {
-//   FunctionVariableParsers parser;
-//   ParseStatus result = parser.do_parse("victor : Integer, prabal ", 0);
+TEST(TupleCreationParser, createTuple2) {
+  TupleAssignmentParser parser;
+  ParseStatus result = parser.do_parse("var z : int[5 + 7 + z];", 0);
 
-//   EXPECT_EQ(result.status, true);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 17);
-//   EXPECT_EQ(result.errorType, "");
-//   EXPECT_EQ(result.remainingCharacters, " prabal ");
-//   EXPECT_EQ(result.parsedCharacters, "victor:Integer,");
-// }
+  EXPECT_EQ(result.status, true);
+  EXPECT_EQ(result.startCharacter, 0);
+  EXPECT_EQ(result.endCharacter, 23);
+  EXPECT_EQ(result.remainingCharacters, "");
+  EXPECT_EQ(result.parsedCharacters, "varz:int[5+7+z];");
+}
 
-// TEST(FunctionHelpers, successFunctionArguments) {
-//   FunctionArgumentParsers parser;
-//   ParseStatus result = parser.do_parse("33 + 2, victor", 0);
+TEST(TupleCreationParser, createTuple3) {
+  TupleAssignmentParser parser;
+  ParseStatus result = parser.do_parse("var z : int[5 * 2 + z];", 0);
 
-//   EXPECT_EQ(result.status, true);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 14);
-//   EXPECT_EQ(result.remainingCharacters, "");
-//   EXPECT_EQ(result.parsedCharacters, "33+2,victor");
-// }
-
-// TEST(FunctionHelpers, successFunctionArguments2) {
-//   FunctionArgumentParsers parser;
-//   ParseStatus result = parser.do_parse("joj, victor,", 0);
-
-//   EXPECT_EQ(result.status, true);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 12);
-//   EXPECT_EQ(result.errorType, "");
-//   EXPECT_EQ(result.remainingCharacters, "");
-//   EXPECT_EQ(result.parsedCharacters, "joj,victor,");
-// }
-
-// TEST(FunctionDeclParser, successFunctionDec1) {
-//   FunctionDeclParser parser;
-//   ParseStatus result = parser.do_parse(
-//       "func victor(prabal :Integer, Jason : String) -> String {if(x == 2){e = "
-//       "2;} else {f = 3;}}",
-//       0);
-
-//   EXPECT_EQ(result.status, true);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 89);
-//   EXPECT_EQ(result.remainingCharacters, "");
-//   EXPECT_EQ(result.parsedCharacters,
-//             "funcvictor(prabal:Integer,Jason:String)->String{if(x==2){e=2;}"
-//             "else{f=3;}}");
-//   EXPECT_EQ(result.errorType, "");
-
-//   PrintVisitor *a = new PrintVisitor();
-//   result.ast->Visit(a);
-//   std::string output = a->GetOutput();
-//   EXPECT_EQ(output, "funcvictor(prabal Jason ){}");
-// }
-
-// TEST(FunctionDeclParser, successFunctionDec2) {
-//   FunctionDeclParser parser;
-//   ParseStatus result = parser.do_parse(
-//       "func victor() -> String {if(x == 2){e = 2;} else {f = 3;}}", 0);
-
-//   EXPECT_EQ(result.status, true);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 58);
-//   EXPECT_EQ(result.remainingCharacters, "");
-//   EXPECT_EQ(result.parsedCharacters,
-//             "funcvictor()->String{if(x==2){e=2;}else{f=3;}}");
-//   EXPECT_EQ(result.errorType, "");
-
-//   PrintVisitor *a = new PrintVisitor();
-//   result.ast->Visit(a);
-//   std::string output = a->GetOutput();
-//   EXPECT_EQ(output, "funcvictor(){victorif (x == 2) {e = 2} else {f = 3}}");
-// }
-
-// TEST(FunctionDeclParser, failFunctionDec1) {
-//   FunctionDeclParser parser;
-//   ParseStatus result =
-//       parser.do_parse("func victor(prabal : Integer) {x + 2}", 0);
-
-//   EXPECT_EQ(result.status, false);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 0);
-//   EXPECT_EQ(result.errorType, "Expecting character: -");
-// }
-
-// TEST(FunctionDeclParser, failFunctionDec2) {
-//   FunctionDeclParser parser;
-//   ParseStatus result = parser.do_parse("func victor(pra) {x + 2}", 0);
-
-//   EXPECT_EQ(result.status, false);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 0);
-//   EXPECT_EQ(result.errorType, "Expecting character: )");
-// }
-
-// TEST(FunctionDeclParser, successFunctionCall1) {
-//   FunctionCallParser parser;
-//   ParseStatus result =
-//       parser.do_parse("return_val = victor(2+3*(2)+victor, jason)", 0);
-
-//   EXPECT_EQ(result.status, true);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 42);
-//   EXPECT_EQ(result.remainingCharacters, "");
-//   EXPECT_EQ(result.parsedCharacters, "return_val=victor(2+3*(2)+victor,jason)");
-//   EXPECT_EQ(result.errorType, "");
-
-//   PrintVisitor *a = new PrintVisitor();
-//   result.ast->Visit(a);
-//   std::string output = a->GetOutput();
-//   EXPECT_EQ(output, "return_val = victor ( ((2 + (3 * 2)) + victor)jason ); ");
-// }
-
-// TEST(FunctionDeclParser, successFunctionCall2) {
-//   FunctionCallParser parser;
-//   ParseStatus result = parser.do_parse("test = victor()", 0);
-
-//   EXPECT_EQ(result.status, true);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 15);
-//   EXPECT_EQ(result.remainingCharacters, "");
-//   EXPECT_EQ(result.parsedCharacters, "test=victor()");
-//   EXPECT_EQ(result.errorType, "");
-
-//   PrintVisitor *a = new PrintVisitor();
-//   result.ast->Visit(a);
-//   std::string output = a->GetOutput();
-//   EXPECT_EQ(output, "test = victor (  ); ");
-// }
-
-// TEST(FunctionDeclParser, failFunctionCall1) {
-//   FunctionCallParser parser;
-//   ParseStatus result = parser.do_parse("test = jason(if victor)", 0);
-
-//   EXPECT_EQ(result.status, false);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 0);
-//   EXPECT_EQ(result.errorType, "Expecting character: )");
-// }
-
-// TEST(FunctionDeclParser, failFunctionCall2) {
-//   FunctionCallParser parser;
-//   ParseStatus result = parser.do_parse("word(yo, 3", 0);
-
-//   EXPECT_EQ(result.status, false);
-//   EXPECT_EQ(result.startCharacter, 0);
-//   EXPECT_EQ(result.endCharacter, 0);
-//   EXPECT_EQ(result.errorType, "Missing equal sign");
-// }
+  EXPECT_EQ(result.status, true);
+  EXPECT_EQ(result.startCharacter, 0);
+  EXPECT_EQ(result.endCharacter, 23);
+  EXPECT_EQ(result.remainingCharacters, "");
+  EXPECT_EQ(result.parsedCharacters, "varz:int[5*2+z];");
+}
